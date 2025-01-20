5528183ec5f2f4fcb9a8972c17c7cd5a297b102b
TwoRedBean <89733180+TwoRedBean@users.noreply.github.com>
2025-01-20 20:26:31 +0800

    Initial commit

---
+++ src/main/java/com/project/news_page/NewsPageApplication.java
@@ -0,0 +1,13 @@
+package com.project.news_page;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsPageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsPageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/news_page/NewsPageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.news_page;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsPageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
