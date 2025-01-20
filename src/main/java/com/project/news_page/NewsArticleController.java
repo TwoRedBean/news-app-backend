cf480302edb3493bd670ab6d1e47de25425d5a88
TwoRedBean <89733180+TwoRedBean@users.noreply.github.com>
2025-01-20 21:51:39 +0800

    add service for better maintainability

--- src/main/java/com/project/news_page/NewsArticleController.java
+++ src/main/java/com/project/news_page/NewsArticleController.java
@@ -1,5 +1,6 @@
 package com.project.news_page;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,20 +11,19 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/articles")
 public class NewsArticleController {
-    private final NewsArticleRepository newsArticleRepository;
+    private final NewsArticleService newsArticleService;
 
-    public NewsArticleController(NewsArticleRepository newsArticleRepository) {
-        this.newsArticleRepository = newsArticleRepository;
+    public NewsArticleController(NewsArticleService newsArticleService) {
+        this.newsArticleService = newsArticleService;
     }
 
     @GetMapping
     public List<NewsArticle> getAllArticles() {
-        return newsArticleRepository.findAll();
+        return newsArticleService.getAllArticles();
     }
 
     @GetMapping(path = "/{id}")
     public NewsArticle getAllArticles(@PathVariable Integer id) {
-        return newsArticleRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Article " + id + " not available"));
+        return newsArticleService.getArticlesById(id);
     }
 }
---
+++ src/main/java/com/project/news_page/NewsArticleService.java
@@ -0,0 +1,24 @@
+package com.project.news_page;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class NewsArticleService {
+    private final NewsArticleRepository newsArticleRepository;
+
+    public NewsArticleService(NewsArticleRepository newsArticleRepository) {
+        this.newsArticleRepository = newsArticleRepository;
+    }
+
+    public List<NewsArticle> getAllArticles() {
+        return newsArticleRepository.findAll();
+    }
+
+    public NewsArticle getArticlesById(Integer id) {
+        return newsArticleRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Article " + id + " not available"));
+    }
+}
